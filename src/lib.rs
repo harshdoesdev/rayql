6d2445c66e3f32bff35998d5f8ee420a9fbc0e32
Harsh Singh <harshsingh.js@gmail.com>
2024-03-16 22:29:34 +0530

    refactor: moved sql generator code to sql::to_sql

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 extern crate self as rayql;
 
 mod schema;
-pub mod sql;
 pub use schema::Schema;
-
+mod sql;
+pub use sql::to_sql;
 pub mod error;
-
 pub mod types;
-
 mod value;
 pub use value::Value;
