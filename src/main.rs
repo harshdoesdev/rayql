5f038de54fa3b368e6cab44b340be2ee6ab8b2c7
Harsh Singh <harshsingh.js@gmail.com>
2024-03-22 01:37:07 +0530

    start implementing cli errors

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use clap::Parser;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = rayql::cli::Cli::parse();
 
     match cli.command {
-        Some(rayql::cli::Commands::Print) => rayql::sql::print_schema(),
-        Some(rayql::cli::Commands::Db(db_args)) => match db_args.command {
-            Some(rayql::cli::DbCommands::Push) => (),
-            None => (),
+        Some(rayql::cli::Commands::Print) => {
+            rayql::sql::print_schema();
+            
+            Ok(())
         },
-        None => (),
+        Some(rayql::cli::Commands::Db(db_args)) => match db_args.command {
+            Some(rayql::cli::DbCommands::Push) => Ok(()),
+            None => Ok(()),
+        },
+        None => Ok(()),
     }
 }
